e64db3160a5890cbf924899bace0b04fcad63954
= <=>
2021-04-29 14:00:00 +0200

    removed code smells

--- app/src/main/java/markus/wieland/trackmaniacotdapp/ui/about/AboutFragment.java
+++ app/src/main/java/markus/wieland/trackmaniacotdapp/ui/about/AboutFragment.java
@@ -1,9 +1,15 @@
 package markus.wieland.trackmaniacotdapp.ui.about;
 
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
 import markus.wieland.defaultappelements.uielements.fragments.DefaultFragment;
 import markus.wieland.trackmaniacotdapp.R;
 
 public class AboutFragment extends DefaultFragment {
+
+    private TextView textViewCredits;
+    private TextView textViewQuestions;
 
     public AboutFragment() {
         super(R.layout.fragment_about);
@@ -11,16 +17,18 @@ public class AboutFragment extends DefaultFragment {
 
     @Override
     public void bindViews() {
-
+        textViewCredits = findViewById(R.id.fragment_about_credits);
+        textViewQuestions = findViewById(R.id.fragment_about_questions);
     }
 
     @Override
     public void initializeViews() {
-
+        textViewCredits.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewQuestions.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
     public void execute() {
-
+        // Nothing to do here
     }
 }
